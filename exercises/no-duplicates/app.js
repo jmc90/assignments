169ee6dabd33e9ca12ded888ba99d0e78447f8b1
Jon <jonathan.mcneil.90@gmail.com>
2018-10-22 20:19:44 -0600

    adding challenge

---
+++ exercises/no-duplicates/app.js
@@ -0,0 +1,14 @@
+// Given a string, your task is to remove any duplicate letters. Any letters removed should be added to another string called "duplicates".
+
+// Create function that accepts a string.
+// Create variable that stores the string without duplicates.
+// Create a variable that stores the letters removed.
+// Have the function return the string without duplicate letters
+// Create another function that returns the letters removed.
+
+function noDuplicates(str) {
+    var strArray = str.split("")
+    for (var i = 0; i < strArray.length; i++) {
+        
+    }
+}
