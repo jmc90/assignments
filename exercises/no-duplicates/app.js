fddde654a1fe115eaa0c198ab37a69334203a11a
Jon <jonathan.mcneil.90@gmail.com>
2018-10-22 21:51:31 -0600

    finished no-duplicates challenge

--- exercises/no-duplicates/app.js
+++ exercises/no-duplicates/app.js
@@ -6,9 +6,44 @@
 // Have the function return the string without duplicate letters
 // Create another function that returns the letters removed.
 
+
+var returnArray = []
+var lettersRemoved = []
+
 function noDuplicates(str) {
     var strArray = str.split("")
     for (var i = 0; i < strArray.length; i++) {
-        
-    }
+        if (returnArray.indexOf(strArray[i]) === -1) {
+            returnArray.push(strArray[i])
+        }  else {
+            lettersRemoved.push(strArray[i])
+        }
+    } 
+    return returnArray.join("")
 }
+
+ console.log(noDuplicates("bookkeeper"))
+
+
+function theDuplicates() {
+    return lettersRemoved.join("")
+}
+
+console.log(theDuplicates())
+
+
+// same challenge solved with .filter()
+
+function noDuplicates(str) {
+        var strArr = str.split("")
+        var noDups = strArr.filter((item, index, arr) => arr.indexOf(item) === index)
+        return noDups.join("")
+}
+
+function duplicates(str) {
+    var strArr = str.split("")
+    var dups = strArr.filter((item, index, arr) => arr.indexOf(item) !== index)
+    return dups.join("")
+}
+
+duplicates("bookkeeper")
