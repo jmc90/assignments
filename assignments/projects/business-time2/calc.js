d490824892969f6ec0e4ab750707f811a77120be
Jon <jonathan.mcneil.90@gmail.com>
2018-10-29 09:01:13 -0600

    business-time2

--- assignments/projects/business-time2/calc.js
+++ assignments/projects/business-time2/calc.js
@@ -1,8 +1,5 @@
-//var disable== ("calculate");
-//song.attr('disabled', true);
-//$("#calculate").attr('disabled', true);
-//document.getElementById("calculate").disabled = true;
-$(document).ready(function() {//$("#calculate").attr('disabled', true);
+
+$(document).ready(function() {
    $('.input-group input[required], .input-group textarea[required], .input-group select[required]').on('keyup, change', function() {
         var $group = $(this).closest('.input-group'),
 			$addon = $group.find('.input-group-addon'),
@@ -35,37 +32,9 @@ $(document).ready(function() {//$("#calculate").attr('disabled', true);
 });
 
 
-/*	function b(a){
-		var b=$("#gain_loss_amount");
-		b.empty();$.each(a,function(a,c){
-			var d={value:c};
-			0===c&&(d.selected="selected");
-			b.append($("<option></option>").attr(d).text(a))
-		})}
-		var a={"Lose 2 Pounds per Week":-1000,"Lose 1.5 Pounds per Week":-750,"Lose 1 Pounds per Week":-500,"Lose 0.5 Pounds per Week":-250,"Stay the Same Weight":0,"Gain 0.5 Pound per Week":250,"Gain 1 Pound per Week":500,"Gain 1.5 Pounds per Week":750,"Gain 2 Pounds per Week":1E3},
-		var c={"Lose 1 Kg per Week":-1100,"Lose 0.75 Kg per Week":-825,"Lose 0.5 Kg per Week":-550,"Lose 0.25 Kg per Week":-275,"Stay the Same Weight":0,"Gain 0.25 Kg per Week":275,"Gain 0.5 Kg per Week":550,"Gain 0.75 Kg per Week":825,"Gain 1 Kg per Week":1100};
-			});
-*/
-
-/* function validateDailyCalsValues(b){
-	var a="";$.isNumeric($("#age").val())||(a+="Age value must be a number\n");
-	$.isNumeric($("#weight").val())||(a+="Weight value must be a number\n");
-	$.isNumeric($("#inches").val())||(a=a+(b?"Feet ":"Height ")+"value must be a number\n");
-//    (!$("activity_level").val())(a="balls")
-//	var c=$("#inches").val();b&&(!$.isNumeric(c)&&!(12>parseFloat(c)))&&(a+="Inches value must be a number less than 12\n");
-	return a
-}
-*/
 function calcDailyCals(){
-	//var b="standard"===$("input[name='units']:checked").val(),
-//    a=validateDailyCalsValues(b);
-//	if(a)alert(a);
-    //if(state)alert("Fill in all values");
-//  if(a)alert("Fill in all fields!");
-//	else{
 		var a=parseFloat($("#weight").val());
 		b=(a*=0.453592);
-		//var c=parseFloat($("#feet_cm").val());
         var c=parseFloat($("#inches").val());
 		b=(c=2.54*c);
 		var d=parseFloat($("#age").val()),e=$("input[name='sex']:checked").val(),b=$("#activity_level").val(),
@@ -74,10 +43,9 @@ function calcDailyCals(){
 		a=Math.round(a+parseInt($("#gain_loss_amount").val()));
         $("#calAmount").text(500<a?a:0);
 		$("#dc_results").show();
-        //if(a>0)
-         //   $("#recommendations").show();
+        
         	
-}//}
+}
 
 function resetForm(){
     $(':input').not(':button, :submit, :reset, :hidden, :checkbox, :radio').val('');
