d527358964c8c39f895cbe97251132cec3662bfb
Jon <jonathan.mcneil.90@gmail.com>
2018-10-30 19:21:58 -0600

    letter-frequency challenge

---
+++ assignments/exercises/letter-frequency/app.js
@@ -0,0 +1,38 @@
+// Find the frequency of all characters in a given word or phrase, and print them to the console.
+
+// Contain the characters and their counts within an object.
+// Each character will be its own key
+// Each value will be the character's frequency.
+
+const phrase = 'slimy smelly solution'
+
+function arrayIndex(str) {
+   var arr = phrase.split('')
+   var obj = {}
+   arr.forEach(char => obj[char] = (obj[char] || 0) + 1)
+   return obj
+}
+
+// console.log(arrayIndex(phrase))
+
+// 
+function getFrequency(string) {
+    var freq = {}
+    for (var i=0; i<string.length;i++) {
+        var character = string.charAt(i);
+        if (freq[character]) {
+           freq[character]++
+        } else {
+           freq[character] = 1
+        }
+    }
+
+    return freq
+}
+
+
+
+
+
+
+
