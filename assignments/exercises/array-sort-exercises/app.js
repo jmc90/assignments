c1918d332fd9bec9d50030baf92369d95e234197
Jon <jonathan.mcneil.90@gmail.com>
2018-11-01 09:13:45 -0600

    sort exercise and rpg project work

---
+++ assignments/exercises/array-sort-exercises/app.js
@@ -0,0 +1,64 @@
+// 1) Sort an array from smallest number to largest
+
+var array = [1, 3, 5, 2, 90, 20]
+
+function leastToGreatest(arr) {
+    return arr.sort((a, b) => a - b)
+}
+
+console.log(leastToGreatest(array))
+
+// 2) Sort an array from largest number to smallest
+
+function greatestToLeast(arr) {
+    return arr.sort((a, b) => b - a)
+}   
+
+console.log(greatestToLeast(array))
+
+// 3) Sort an array from shortest string to longest
+
+var strings = ["dog", "wolf", "by", "family", "eaten"]
+
+
+function lengthSort(arr) {
+    return arr.sort((a, b) => a.length - b.length)
+}
+
+
+console.log(lengthSort(strings))
+
+
+// 4) Sort an array alphabetically
+
+function alphabetical(arr) {
+    return arr.sort()
+}
+
+console.log(alphabetical(strings))
+
+
+// 5) Sort the objects in the array by age
+
+var people = [
+    {
+        name: "Quiet Samurai",
+        age: 22
+    },
+    {
+        name: "Arrogant Ambassador",
+        age: 100
+    },
+    {
+        name: "Misunderstood Observer",
+        age: 2
+    },
+    {
+        name: "Unlucky Swami",
+        age: 77
+    }
+]
+
+function byAge(arr){
+
+}
