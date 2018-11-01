9c0ceff73587d02a63fbc9fd8907c3c1f752f89b
Jon <jonathan.mcneil.90@gmail.com>
2018-11-01 17:01:43 -0600

    rpg

--- assignments/projects/console-rpg/app.js
+++ assignments/projects/console-rpg/app.js
@@ -3,11 +3,7 @@ var readline = require('readline-sync')
 
 // REQUIREMENTS 
  /*
- character creation
-walking
-running from a fight
 fighting
-enemy creation
 attacking enemy
 enemy attacking
 enemy dying (including dropping an item)
@@ -42,10 +38,11 @@ function Enemy(type, hp){
 ///////////////////////////
 //---Global Variables---//
 var player1 = new Player("Jon", 100)
+var enemy = new Enemy(alien, 50)
 var hasWon = false
 var walkDistance = 0
 var triedRun = false
-var alienTypes = []
+var alienTypes = ["Grey", "Reptilian", "Chupacabra"]
 var alienWeapons =[]
 
 
@@ -55,22 +52,27 @@ var alienWeapons =[]
 ///////////////////////////
 //---Functions---//
 
-function walk(){
-    var toDo = readline.keyIn("Press 'w' to walk or 'i' to check inventory", {limit: 'wi'})
-     if (toDo === 'w'){
-         if(Math.floor((Math.random() * 3) + 1) === 1){
-             console.log("fight function here")
-             walkDistance++
-         } else {
-             console.log("Luckily you ran into no aliens")
-             //tell user they didn't run into a monster
-             //****THIS PART IS IMPORTANT. DO NOT CALL WALK()****
-         }
-     } else if (toDo === 'i') {
-         //tell user what's in their inventory, and their health, and then tell them to push w to walk
-         console.log(player1.inventory)
-    } else {
-        console.log("Choose a correct key to continue")
+function playerAttacksEnemy() {
+    var pAttackDamage = player1.attack()
+    enemy.hp -= pAttackDamage
+    console.log(`Good shot! Enemy hp is now ${enemy.hp}`)
+}
+
+function enemyAttacksPlayer() {
+    var eAttackDamage = enemy.attack()
+    player1.hp -= eAttackDamage
+    console.log(`You have been hit! Your hp is now ${player1.hp}`)
+}
+
+function attackSequence() {
+    while(player1.hp > 0 && enemy.hp > 0) {
+        playerAttacksEnemy()
+        enemyAttacksPlayer()
+    }
+    if (player.hp <= 0) {
+        console.log("YOUR'E DEAD!!!")
+    } else if (enemy.hp <= 0) {
+        console.log("You took him out! Lets keep going!")
     }
 }
 
@@ -87,16 +89,41 @@ function run(){
     trieRun = true
 }
 
+
 function fight(){
     var fightOrRun = readline.keyIn("Press 'r' to run or 'f' to fight", {limit: 'rf'} )
-    var enemy = new Enemy(alien, 50) ///change to random enemy and hp.
+    enemy = new Enemy(alienTypes[Math.floor(Math.random() * 3)], 50) ///change to random enemy and hp.
     if (fightOrRun === 'r'){
         run()
     } else {
-        attackEnemy
+        attackSequence()
     }
+    return triedRun = false
 }
 
+function walk(){
+    var toDo = readline.keyIn("Press 'w' to walk or 'i' to check inventory", {limit: 'wi'})
+     if (toDo === 'w'){
+         if(Math.floor((Math.random() * 3) + 1) === 1){
+             fight()
+             walkDistance++
+         } else {
+             console.log("Luckily you ran into no aliens")
+             //tell user they didn't run into a monster
+             //****THIS PART IS IMPORTANT. DO NOT CALL WALK()****
+         }
+     } else if (toDo === 'i') {
+         if (player1.inventory.length === 0) {
+             console.log("You're inventory is EMPTY!!")
+         } else {
+            //tell user what's in their inventory, and their health, and then tell them to push w to walk
+         console.log(`Your inventory contains: ${player1.inventory}`)
+         }
+         
+    } else {
+        console.log("Choose a correct key to continue")
+    }
+}
 
 //////////////////////////////
 
