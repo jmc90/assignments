0138ea859034b0a8c476580f368c072449feace6
Jon <jonathan.mcneil.90@gmail.com>
2018-11-06 13:37:35 -0700

    to do list get features

--- assignments/projects/todo-list/app.js
+++ assignments/projects/todo-list/app.js
@@ -1,6 +1,10 @@
+var list = document.getElementById('list-container')
+
+
 function getData(){
     axios.get('https://api.vschool.io/Jon/todo/').then(function(response){
         listTodos(response.data)
+        console.log(response.data)
     })
 }
 
@@ -9,24 +13,47 @@ function listTodos(arr){
 
         // Create container for specific Todo
         var todoContainer = document.createElement('div')
-            // Add a class to that container (if you want)
+        // Add a class to that container (if you want)
         todoContainer.classList.add('todo')
-
         // Create an html element
         var title = document.createElement('h3')
         var description = document.createElement('p')
-
+        var button = document.createElement('button')
         // Put the Todo title inside of that element
         title.textContent = arr[i].title
         description.textContent = arr[i].description
+        button.textContent = "Delete"
+        //strike through
+        if (arr[i].completed) {
+            title.classList.add('done')
+        } 
+        
         // Put element on the DOM
         todoContainer.appendChild(title)
         todoContainer.appendChild(description)
+        todoContainer.appendChild(button)
 
-        document.getElementById('list-container').appendChild(todoContainer)
+        list.appendChild(todoContainer)
+
+
     }
 }
 
-
+// Step 2: Post a new Todo
+var form = document.todoForm
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+    // When the user submits
+        // Grab the user input
+    var title = todoForm.title.value
+        // Put that input in a new object
+    var newTodo = {}
+    newTodo.title = title
+        // Send a Post request
+    axios.post('https://api.vschool.io/Jon/todo', newTodo).then(function(response){
+        console.log(response.data) // should be new todo with an _id added
+        // Then refresh page to see item added to existing list.
+    })
+})
 
 getData()
