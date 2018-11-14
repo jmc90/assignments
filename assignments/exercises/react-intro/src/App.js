fa3848dd333ca2532e608876f96e35247fd0c287
Jon <jonathan.mcneil.90@gmail.com>
2018-11-14 14:42:59 -0700

    react intro

---
+++ assignments/exercises/react-intro/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Hello World</h1>
+            <p>Other text</p>
+        </div>
+    )
+}
+
+export default App
---
+++ assignments/exercises/react-intro/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App'
+import './style.css'
+
+
+
+        //1: What to render. 2: Where to render
+ReactDom .render(<App />, document.getElementById('root'))
+
+
+// Functional / Display components
+
+
