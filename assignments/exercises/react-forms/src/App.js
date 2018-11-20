102fc8e65520d07b73e01e3cd6afa4ed63b9538a
Jon <jonathan.mcneil.90@gmail.com>
2018-11-20 11:28:22 -0700

    react forms

--- assignments/exercises/react-forms/src/App.js
+++ assignments/exercises/react-forms/src/App.js
@@ -1,39 +1,62 @@
 import React, { Component } from 'react';
 
-
 class App extends Component {
   constructor() {
     super()
     this.state = {
       petFirstName: '',
-      petLastName: ''
+      petLastName: '',
+      pets: []
     }
   }
 
   handleChange = event => {
-    this.setState({[event.target.name]: event.target.value})
+    this.setState({ [event.target.name]: event.target.value })
   }
-  
+
   handleSubmit = event => {
     event.preventDefault()
     alert(`A pet's name was Submitted: ${this.state.petFirstName} ${this.state.petLastName}`)
+    const petNames = {
+      first: this.state.petFirstName,
+      last: this.state.petLastName
+    }
+    this.setState(prevState => {
+      return {
+        petFirstName: "",
+        petLastName: "",
+        pets: [...prevState.pets, petNames]}
+    })
   }
 
+
+
   render() {
+
+    const mappedNames = this.state.pets.map(pet => 
+                        <li>{pet.first} {pet.last}</li>)
+
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text"
-               placeholder="Pet First Name"
-               value={this.state.petFirstName}
-               name="petFirstName" 
-               onChange={this.handleChange} />
-        <input type="text"
-               placeholder="Pet last Name"
-               value={this.state.petLastName}
-               name="petLastName" 
-               onChange={this.handleChange} />
-        <button>Submit</button>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text"
+            placeholder="Pet First Name"
+            value={this.state.petFirstName}
+            name="petFirstName"
+            onChange={this.handleChange} />
+          <input type="text"
+            placeholder="Pet last Name"
+            value={this.state.petLastName}
+            name="petLastName"
+            onChange={this.handleChange} />
+          <button>Submit</button>
+        </form>
+        <h1>{this.state.petFirstName} {this.state.petLastName}</h1>
+        <h2>Pets</h2>
+        <ol>
+          {mappedNames}
+        </ol>
+      </div>
     );
   }
 }
