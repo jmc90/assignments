6bea56e93af8a0801eac85fe2ccd2139dad0d2f0
Jon <jonathan.mcneil.90@gmail.com>
2018-11-26 11:56:20 -0700

    added components

--- assignments/exercises/react-roto-router/src/App.js
+++ assignments/exercises/react-roto-router/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { Switch, Route } from "react-router-dom"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import Home from "./Home"
+import About from "./About"
+import Services from "./Services"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-      
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+        </Switch>
+        <Footer />
+
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ assignments/exercises/react-roto-router/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="nav">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/services">Services</Link>
+        </div>
+    )
+}
+
+export default Footer
