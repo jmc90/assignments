4cec89c3fe4038fbaf3ea9e62a2096e8cf53237a
Jon <jonathan.mcneil.90@gmail.com>
2018-11-26 11:12:29 -0700

    update

---
+++ assignments/exercises/react-todo-list/src/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TodoItem = () => {
+  return (
+    <div className="todo-item">
+        <input type="checkbox"/>
+        <p>PlaceHolder text here</p>
+    </div>
+  )
+}
+
+export default TodoItem
