685b550ea8fe01f0601addfa34af39755e5390a0
Jon <jonathan.mcneil.90@gmail.com>
2018-11-27 18:03:49 -0700

    update

---
+++ assignments/exercises/balance-parantheses/app.js
@@ -0,0 +1,5 @@
+// Given a string including parentheses, write a function that returns true if every opening parentheses has a closing parentheses.
+
+function balanceParentheses(str){
+    // Your code here
+}
