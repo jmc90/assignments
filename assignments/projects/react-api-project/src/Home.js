a0b6bcfe501fd4d9255e8983a0e74dc18a6281db
Jon <jonathan.mcneil.90@gmail.com>
2018-12-04 11:29:01 -0700

    navbar

--- assignments/projects/react-api-project/src/Home.js
+++ assignments/projects/react-api-project/src/Home.js
@@ -5,9 +5,10 @@ import logo from './styles/images/nasa.png'
 const Home = () => {
   return (
     <div className="home">
-        <div className="home-title-div">
-          <div className="title">
-            <img src={logo} alt="nasa"/>
+        <div className="home-title-div d-flex justify-content-center align-items-center">
+          <div className="title d-flex flex-column align-items-center">
+            <img src={logo} alt="nasa" width="400"/>
+            <h1 className="display-3">Image Gallery Powered by The Nasa API</h1>
           </div>
         </div>
         <div className="stars"></div>
--- assignments/projects/react-api-project/src/NavbarContainer.js
+++ assignments/projects/react-api-project/src/NavbarContainer.js
@@ -1,15 +1,69 @@
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom"
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem
+   } from 'reactstrap';
 
-const NavbarContainer = () => {
-  return (
-    <div className="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/apod">APOD</Link>
-        <Link to="/rovergallery">Rover Gallery</Link>
+class NavbarContainer extends React.Component {
+  constructor(props) {
+    super(props);
 
-    </div>
-  )
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div >
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Nasa</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse  isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link to="/">Home</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/apod">Picture of The Day</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/rovergallery">Rover Gallery</Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default NavbarContainer
+
+
+
+// import React from 'react'
+// import { Link } from "react-router-dom"
+
+// const NavbarContainer = () => {
+//   return (
+//     <div className="navbar">
+//         <Link to="/">Home</Link>
+//         <Link to="/apod">Picture of The Day</Link>
+//         <Link to="/rovergallery">Rover Gallery</Link>
+
+//     </div>
+//   )
+// }
+
+// export default NavbarContainer
