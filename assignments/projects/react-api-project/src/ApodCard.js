d04792a973cb3e915aea5c9292d751c81b706638
Jon <jonathan.mcneil.90@gmail.com>
2018-12-05 00:56:19 -0700

    css

--- assignments/projects/react-api-project/src/ApodCard.js
+++ assignments/projects/react-api-project/src/ApodCard.js
@@ -12,23 +12,26 @@ const ApodCard = ({ apodData }) => {
   return (
       media_type === "image" ?
         <div className="image-card d-flex flex-column align-items-center">
-            <h3 className="">Date: {date}</h3>
-            <h1 className="">{title}</h1>
+            <h3 className="text-center">Date: {date}</h3>
+            <h1 className="text-center">{title}</h1>
             <a href={hdurl}><img className="img-fluid" src={url} alt={title} /></a>
             <p className="text-center">{explanation}</p>
         </div>
         :
-        <div>
-            <h3>Date: {date}</h3>
-            <h1>{title}</h1>
-            <iframe
-                allowFullScreen
-                title= "apod video"
-                frameBorder="0"
-                height="520"
-                width="720"
-                src={url}>
-            </iframe>
+        <div className="d-flex flex-column align-items-center">
+            <h3 className="text-center">Date: {date}</h3>
+            <h1 className="text-center">{title}</h1>
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe
+                    className="embed-responsive-item"
+                    allowFullScreen
+                    title= "apod video"
+                    frameBorder="0"
+                    height="520"
+                    width="720"
+                    src={url}>
+                </iframe>
+            </div>
             <p>{explanation}</p>
         </div>
   )
