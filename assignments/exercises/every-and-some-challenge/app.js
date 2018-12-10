6494ae395d45a8e405d24ec01d2e048cf875a6d1
Jon <jonathan.mcneil.90@gmail.com>
2018-12-10 01:39:09 -0700

    challenge complete

---
+++ assignments/exercises/every-and-some-challenge/app.js
@@ -0,0 +1,42 @@
+// Write two functions that mimic the .every and .some array methods.
+
+// every(arr, callback): Given an array and a callback function, return true or false based on whether each element of the array, when inserted as a parameter to the callback, returns true.
+
+function every(arr, callback) {
+    let result
+    for(let i = 0; i < arr.length; i++) {
+      if(callback(arr[i]) === true) {
+        result = true
+      } else {
+        return false
+      }
+    }
+    return result
+  }
+  
+console.log(every([1,2, 3], (num)=>{
+    return typeof num === "number";
+    }))
+
+  
+// some(arr, callback): Given an array and a callback function, return true or false based on whether at least ONE element of the array, when inserted as a parameter to the callback, returns true.
+
+function some(arr, callback) {
+    let result
+    for(let i = 0; i < arr.length; i++) {
+      if(callback(arr[i]) === true) {
+        return true
+      } else {
+        result = false
+      }
+    }
+    return result
+  }
+  
+console.log(some(["ben", "jon", "bob"], (name)=>{
+    return name === "jacob";
+  }));
+
+
+
+
