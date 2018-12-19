836613e7038d360d1eab72d1571f2745453538da
Jon <jonathan.mcneil.90@gmail.com>
2018-12-18 17:28:35 -0700

    added register form

--- assignments/projects/speech-journal/client/src/components/Register/Register.js
+++ assignments/projects/speech-journal/client/src/components/Register/Register.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import RegisterForm from './RegisterForm'
 
 const Register = () => {
   return (
     <div>
-      Register page
+      <RegisterForm />
     </div>
   )
 }
---
+++ assignments/projects/speech-journal/client/src/components/Register/RegisterForm.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const RegisterForm = () => {
+  return (
+    <div>
+        <form onSubmit={}>
+            <input 
+                type="text"
+                name="username"
+                value={} 
+                onChange={} />
+            <input 
+                type="password"
+                name="password"
+                value={} 
+                onChange={} />
+            <button>Register</button>
+        </form>
+    
+    </div>
+  )
+}
+
+export default RegisterForm
