6b5f0909acd62079ff1b67a3425516d3996b1725
Jon <jonathan.mcneil.90@gmail.com>
2018-12-21 17:12:14 -0700

    update

--- assignments/projects/speech-journal/client/src/context/EntryProvider.js
+++ assignments/projects/speech-journal/client/src/context/EntryProvider.js
@@ -12,6 +12,48 @@ class EntryProvider extends Component {
     }
   }
 
+  getUserEntries = userId => {
+    axios.get(`/entry/${userId}`).then(res => {
+      this.setState({
+        entries: res.data
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
+  getSingleEntry = entryId => {
+    axios.get(`/entry/${entryId}`).then(res => {
+      this.setState({
+        singleEntry: res.data
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
+  addEntry = (entry, userId) => {
+    axios.post(`/entry/${userId}`, entry).then(res => {
+      this.setState(prevState => {
+        return {
+          entries: [...prevState.entries, res.data]
+        }
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
+  deleteEntry = entryId => {
+    axios.delete(`/entry/${entryId}`).then(res => {
+      if (res.data === "Successfully deleted entry!") {
+        this.setState(prevState => {
+          return {
+            entries: prevState.entries.filter(entry => entry._id !== entryId)
+          }
+        })
+      }
+    })
+    .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <EntryContext.Provider
