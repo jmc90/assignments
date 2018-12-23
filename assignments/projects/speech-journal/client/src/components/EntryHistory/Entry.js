78eb64703ef44bffa5b6e0cbb22f275438fbc145
Jon <jonathan.mcneil.90@gmail.com>
2018-12-22 23:31:39 -0700

    created entry component

---
+++ assignments/projects/speech-journal/client/src/components/EntryHistory/Entry.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { withEntry } from '../../context/EntryProvider'
+import { withUser } from '../../context/UserProvider'
+
+class Entry extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Entry
