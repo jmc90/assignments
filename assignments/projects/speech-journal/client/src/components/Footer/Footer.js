df07b5c605dcd93a265e6c4d334586352d633cc7
Jon <jonathan.mcneil.90@gmail.com>
2018-12-24 22:09:24 -0700

    styling

--- assignments/projects/speech-journal/client/src/components/Footer/Footer.js
+++ assignments/projects/speech-journal/client/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      Footer
+      <p className="text-center text-white p-3 m-0">Created by Jon McNeil &copy;2018</p>
     </div>
   )
 }
