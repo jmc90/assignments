3adf01f181adcfbd428712d3ebe7ca86e9994b0b
Jon <jonathan.mcneil.90@gmail.com>
2018-12-24 22:10:24 -0700

    baclground

--- assignments/projects/speech-journal/client/src/App.js
+++ assignments/projects/speech-journal/client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import { Switch, Route, Redirect } from "react-router-dom"
 import { withUser } from './context/UserProvider'
-import Navbar from "./components/Navbar/Navbar"
+import NavbarContainer from "./components/Navbar/NavbarContainer"
 import Footer from "./components/Footer/Footer"
 import LandingPage from "./components/LandingPage/LandingPage"
 import SignIn from "./components/SignIn/SignIn"
@@ -9,6 +9,7 @@ import Register from "./components/Register/Register"
 import Journal from "./components/Journal/Journal"
 import EntryHistory from "./components/EntryHistory/EntryHistory"
 import Entry from "./components/EntryHistory/Entry"
+import './App.css'
 
 
 
@@ -20,7 +21,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <NavbarContainer />
         <Switch>
           <Route exact path="/" render={routerProps => 
                                           this.props.isAuthenticated 
